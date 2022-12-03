7e19bec63aaa10730a3a131999842c39b09d5027
Joseph Butler <34063225+butlerjoe51@users.noreply.github.com>
2022-12-03 16:16:37 +0000

    Create Pytube Video Downloader.py

---
+++ Python/Pytube/Pytube Video Downloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube #Install with Pip before running the program
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+    
+link = input("Enter the YouTube video URL: ") #Copy and paste the URL, and you can have the video saved to the directory this program was run from
+Download(link)
